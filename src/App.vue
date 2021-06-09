b09afb9d61cd34ac1f21c421cfe246d7886b2598
Sami Henchiri <sami@fygr.io>
2021-06-09 10:51:50 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainComponent></MainComponent>
+    <div>hello Sami ! Nice job !!</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainComponent
   }
 }
 </script>
@@ -22,7 +22,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
+  height: 50vh;
+  width: 100%;
+  min-width: 100%;
 }
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main_component" :style="image">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+  data() {
+    return {
+      image: { backgroundImage: "url(https://www.tourmag.com/photo/art/grande/35034019-31848358.jpg?v=1561366624)" }
+    };
+  }
+}
+</script>
+
+<style scoped>
+.main_component {
+  margin: 0px;
+  padding: 0px;
+  height: 100% !important;
+  width: 100% !important;
+  background-repeat: no-repeat;
+  background-size: auto;
+  text-align: center;
+  background-position: center;
+}
+</style>
